3eb958a651e054cab40c36db7437be550ea10a96
Joseph Agnelli <jxa4717@rit.edu>
2022-08-13 18:29:47 -0700

    Updates

--- src/components/GameBoard.react.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Chessboard } from 'kokopu-react';
-
-export default function GameBoard(props) {
-	return <Chessboard position={props.position.fen()} squareSize={50} />;
-}
--- src/utils/CalcMoveFunctionContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const CalcMoveFunctionContext = React.createContext(`position => {
-    const legalMoves = position.moves();
-    return legalMoves[Math.floor(Math.random() * legalMoves.length)];
-  }`);
-
-export default CalcMoveFunctionContext;
